2d3ef7f53c78a4035c07660a3ede9abf90089821
Mateusz <echelon133@gmail.com>
2023-02-17 13:38:07 +0100

    Add user service's main class

---
+++ user/src/main/java/ml/echelon133/microblog/user/UserApplication.java
@@ -0,0 +1,16 @@
+package ml.echelon133.microblog.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EntityScan("ml.echelon133.microblog.shared")
+@EnableDiscoveryClient
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
