a1a232e9303e936ca43d371ee11f45003f180ec9
Mateusz <echelon133@gmail.com>
2023-02-25 22:51:05 +0100

    Add a BCryptPasswordEncoder bean for encoding user passwords

--- user/src/main/java/ml/echelon133/microblog/user/UserApplication.java
+++ user/src/main/java/ml/echelon133/microblog/user/UserApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EntityScan({"ml.echelon133.microblog.shared", "ml.echelon133.microblog.user.model"})
@@ -14,5 +17,10 @@ public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
