840b4df625c34389786017f280b99f0a836d409d
Mateusz <echelon133@gmail.com>
2023-02-25 22:49:43 +0100

    Enable Jpa Auditing to make 'CreateDate' annotation work

--- user/src/main/java/ml/echelon133/microblog/user/UserApplication.java
+++ user/src/main/java/ml/echelon133/microblog/user/UserApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan({"ml.echelon133.microblog.shared", "ml.echelon133.microblog.user.model"})
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class UserApplication {
 
     public static void main(String[] args) {
