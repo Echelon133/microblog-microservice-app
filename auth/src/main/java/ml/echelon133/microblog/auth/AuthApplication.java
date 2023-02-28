7fda6fbdab5120080156291fd73f554669820099
Mateusz <echelon133@gmail.com>
2023-02-28 17:01:17 +0100

    Scan the shared library for User & Role entities

--- auth/src/main/java/ml/echelon133/microblog/auth/AuthApplication.java
+++ auth/src/main/java/ml/echelon133/microblog/auth/AuthApplication.java
@@ -2,12 +2,14 @@ package ml.echelon133.microblog.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EntityScan({"ml.echelon133.microblog.shared.user"})
 @EnableWebSecurity
 public class AuthApplication {
 
