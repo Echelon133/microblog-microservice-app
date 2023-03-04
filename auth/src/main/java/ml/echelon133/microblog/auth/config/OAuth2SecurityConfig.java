4cbe5c4d92e87f0e1acd5f595773693145f87676
Mateusz <echelon133@gmail.com>
2023-03-04 23:33:03 +0100

    Configure the confidential client which is required to let other services access the oauth2 token introspection endpoint

--- auth/src/main/java/ml/echelon133/microblog/auth/config/OAuth2SecurityConfig.java
+++ auth/src/main/java/ml/echelon133/microblog/auth/config/OAuth2SecurityConfig.java
@@ -5,18 +5,23 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.core.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
@@ -24,12 +29,27 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 @Configuration
 public class OAuth2SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${CONFIDENTIAL_CLIENT_ID}")
+    private String clientId;
+
+    @Value("${CONFIDENTIAL_CLIENT_SECRET}")
+    private String clientSecret;
+
+    @Autowired
+    public OAuth2SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     @Order(HIGHEST_PRECEDENCE)
@@ -46,18 +66,42 @@ public class OAuth2SecurityConfig {
     }
 
     @Bean
+    public TokenSettings tokenSettings() {
+        return TokenSettings.builder()
+                .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                .accessTokenTimeToLive(Duration.of(3, ChronoUnit.HOURS))
+                .build();
+    }
+
+    @Bean
     public RegisteredClientRepository registeredClientRepository() {
-        // test client for evaluating authentication flows
-        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("client")
-                .clientSecret("{noop}")
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
-                .redirectUri("http://auth:80")
-                .scope(OidcScopes.OPENID)
+
+        /*
+        If the password encoder is not a no-op, not encoding the client secret
+        will result in "invalid_client" error during authentication, because
+        ClientSecretAuthenticationProvider will fail during passwordEncoder.matches()
+        since the plaintext provided during client configuration won't be recognized
+        as an output of a password encoder
+        */
+        var confidentialClientSecret = passwordEncoder.encode(clientSecret);
+        /*
+        Configure the confidential client which will let other services access
+        the token introspection endpoint at /oauth2/introspect. It's impossible
+        to introspect an access token without having another form of authorization,
+        because https://www.rfc-editor.org/rfc/rfc7662 specifies that:
+            "To prevent token scanning attacks, the [introspection] endpoint MUST
+            also require some form of authorization to access this endpoint, such as client
+            authentication"
+         */
+        var confidentialClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId(clientId)
+                .clientSecret(confidentialClientSecret)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings())
                 .build();
 
-        return new InMemoryRegisteredClientRepository(registeredClient);
+        return new InMemoryRegisteredClientRepository(confidentialClient);
     }
 
     @Bean
