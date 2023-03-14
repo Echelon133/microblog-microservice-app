ed5cb37c6cd2bf28c65ac7fc9b8810d750356118
Mateusz <echelon133@gmail.com>
2023-03-14 11:03:14 +0100

    Implement getMe and patchMe controller methods

--- user/src/main/java/ml/echelon133/microblog/user/controller/UserController.java
+++ user/src/main/java/ml/echelon133/microblog/user/controller/UserController.java
@@ -2,17 +2,21 @@ package ml.echelon133.microblog.user.controller;
 
 import ml.echelon133.microblog.shared.user.UserCreationDto;
 import ml.echelon133.microblog.shared.user.UserDto;
+import ml.echelon133.microblog.shared.user.UserUpdateDto;
 import ml.echelon133.microblog.user.exception.UserDataInvalidException;
 import ml.echelon133.microblog.user.exception.UserNotFoundException;
 import ml.echelon133.microblog.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
@@ -49,5 +53,30 @@ public class UserController {
         }
 
         return Map.of("uuid", userService.setupAndSaveUser(dto));
+    }
+
+    @GetMapping("/me")
+    public UserDto getMe(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) throws UserNotFoundException {
+        var id = UUID.fromString(Objects.requireNonNull(principal.getAttribute("token-owner-id")));
+        return userService.findById(id);
+    }
+
+    @PatchMapping("/me")
+    public UserDto patchMe(@Valid @RequestBody(required = false) UserUpdateDto dto,
+                            BindingResult result,
+                            @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) throws Exception {
+
+        var id = UUID.fromString(Objects.requireNonNull(principal.getAttribute("token-owner-id")));
+
+        if (result.hasErrors()) {
+            List<String> errorMessages = result
+                    .getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            throw new UserDataInvalidException(errorMessages);
+        }
+
+        return userService.updateUserInfo(id, dto);
     }
 }
