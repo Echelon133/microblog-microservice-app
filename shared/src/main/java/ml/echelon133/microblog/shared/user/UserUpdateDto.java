67a5dce01a6fdf3ae4e403d53daa355cb5a24178
Mateusz <echelon133@gmail.com>
2023-03-14 09:14:52 +0100

    Add a DTO which stores updatable fields of user

---
+++ shared/src/main/java/ml/echelon133/microblog/shared/user/UserUpdateDto.java
@@ -0,0 +1,45 @@
+package ml.echelon133.microblog.shared.user;
+
+import org.hibernate.validator.constraints.Length;
+
+public class UserUpdateDto {
+
+    @Length(max = 40, message = "Length of displayedName cannot be longer than 40 characters")
+    private String displayedName;
+
+    @Length(max = 200, message = "Length of aviUrl cannot be longer than 200 characters")
+    private String aviUrl;
+
+    @Length(max = 300, message = "Length of description cannot be longer than 300 characters")
+    private String description;
+
+    public UserUpdateDto(String displayedName, String aviUrl, String description) {
+        this.displayedName = displayedName;
+        this.aviUrl = aviUrl;
+        this.description = description;
+    }
+
+    public String getDisplayedName() {
+        return displayedName;
+    }
+
+    public void setDisplayedName(String displayedName) {
+        this.displayedName = displayedName;
+    }
+
+    public String getAviUrl() {
+        return aviUrl;
+    }
+
+    public void setAviUrl(String aviUrl) {
+        this.aviUrl = aviUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
