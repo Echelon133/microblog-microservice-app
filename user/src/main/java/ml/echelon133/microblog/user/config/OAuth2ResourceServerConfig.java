2d72efb298ecf4b906dc78462915fe821f2f77d2
Mateusz <echelon133@gmail.com>
2023-03-14 11:14:13 +0100

    Protect 'PATCH /api/users/me' with USER_READ and USER_WRITE scopes, and not just USER_WRITE

--- user/src/main/java/ml/echelon133/microblog/user/config/OAuth2ResourceServerConfig.java
+++ user/src/main/java/ml/echelon133/microblog/user/config/OAuth2ResourceServerConfig.java
@@ -1,11 +1,13 @@
 package ml.echelon133.microblog.user.config;
 
+import ml.echelon133.microblog.shared.auth.MultiAuthorizationManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -31,7 +33,12 @@ public class OAuth2ResourceServerConfig {
                         .antMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                         .antMatchers(HttpMethod.GET, "/actuator/health/**").permitAll()
                         .antMatchers(HttpMethod.GET, "/api/users/me").hasAuthority(prefix(USER_READ))
-                        .antMatchers(HttpMethod.PATCH, "/api/users/me").hasAuthority(prefix(USER_WRITE))
+                        .antMatchers(HttpMethod.PATCH, "/api/users/me").access(
+                                MultiAuthorizationManager.hasAll(
+                                        AuthorityAuthorizationManager.hasAuthority(prefix(USER_READ)),
+                                        AuthorityAuthorizationManager.hasAuthority(prefix(USER_WRITE))
+                                )
+                        )
                         .antMatchers(HttpMethod.GET, "/api/users/*").hasAuthority(prefix(USER_READ))
                         .anyRequest().denyAll()
                 )
