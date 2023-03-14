244efa24e34c571701c98a884b7425039819e960
Mateusz <echelon133@gmail.com>
2023-03-14 09:48:18 +0100

    Implement tests of updateUserInfo

--- user/src/test/java/ml/echelon133/microblog/user/service/UserServiceTests.java
+++ user/src/test/java/ml/echelon133/microblog/user/service/UserServiceTests.java
@@ -1,11 +1,8 @@
 package ml.echelon133.microblog.user.service;
 
-import ml.echelon133.microblog.shared.user.UserCreationDto;
-import ml.echelon133.microblog.shared.user.UserDto;
+import ml.echelon133.microblog.shared.user.*;
 import ml.echelon133.microblog.user.exception.UserNotFoundException;
 import ml.echelon133.microblog.user.exception.UsernameTakenException;
-import ml.echelon133.microblog.shared.user.Role;
-import ml.echelon133.microblog.shared.user.User;
 import ml.echelon133.microblog.user.repository.RoleRepository;
 import ml.echelon133.microblog.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -23,6 +20,8 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /*
     Disable kubernetes during tests to make local execution of tests possible.
@@ -111,5 +110,91 @@ public class UserServiceTests {
         // no assertions, this test will fail if encode() of password encoder
         // is not called in the setupAndSaveUser, because mockito will complain
         // about unnecessary stubbing of passwordEncoder
+    }
+
+    @Test
+    @DisplayName("updateUserInfo throws a UserNotFoundException when there is no user")
+    public void updateUserInfo_UserDoesNotExist_ThrowsException() {
+        // given
+        UUID uuid = UUID.randomUUID();
+        given(userRepository.existsById(uuid)).willReturn(false);
+
+        // then
+        String message = assertThrows(UserNotFoundException.class, () -> {
+            userService.updateUserInfo(uuid, null);
+        }).getMessage();
+        assertEquals(message, String.format("User with id %s could not be found", uuid));
+    }
+
+    @Test
+    @DisplayName("updateUserInfo updates all fields when all fields provided")
+    public void updateUserInfo_AllFieldsProvided_UpdatesAllFields() throws UserNotFoundException {
+        // given
+        var id = UUID.randomUUID();
+        UserUpdateDto allFields = new UserUpdateDto("new name", "http://test.com", "description");
+        given(userRepository.existsById(id)).willReturn(true);
+
+        // when
+        userService.updateUserInfo(id, allFields);
+
+        // then
+        verify(userRepository, times(1)).updateDisplayedName(id, allFields.getDisplayedName());
+        verify(userRepository, times(1)).updateAviUrl(id, allFields.getAviUrl());
+        verify(userRepository, times(1)).updateDescription(id, allFields.getDescription());
+        verify(userRepository, times(1)).findByUserId(id);
+    }
+
+    @Test
+    @DisplayName("updateUserInfo updates only displayedName if only displayedName provided")
+    public void updateUserInfo_OnlyDisplayedNameProvided_UpdatesOnlyDisplayedName() throws UserNotFoundException {
+        // given
+        var id = UUID.randomUUID();
+        UserUpdateDto onlyDName = new UserUpdateDto("new name", null, null);
+        given(userRepository.existsById(id)).willReturn(true);
+
+        // when
+        userService.updateUserInfo(id, onlyDName);
+
+        // then
+        verify(userRepository, times(1)).updateDisplayedName(id, onlyDName.getDisplayedName());
+        verify(userRepository, times(0)).updateAviUrl(any(), any());
+        verify(userRepository, times(0)).updateDescription(any(), any());
+        verify(userRepository, times(1)).findByUserId(id);
+    }
+
+    @Test
+    @DisplayName("updateUserInfo updates only aviUrl if only aviUrl provided")
+    public void updateUserInfo_OnlyAviUrlProvided_UpdatesOnlyAviUrl() throws UserNotFoundException {
+        // given
+        var id = UUID.randomUUID();
+        UserUpdateDto onlyAvi = new UserUpdateDto(null, "http://test.com", null);
+        given(userRepository.existsById(id)).willReturn(true);
+
+        // when
+        userService.updateUserInfo(id, onlyAvi);
+
+        // then
+        verify(userRepository, times(0)).updateDisplayedName(any(), any());
+        verify(userRepository, times(1)).updateAviUrl(id, onlyAvi.getAviUrl());
+        verify(userRepository, times(0)).updateDescription(any(), any());
+        verify(userRepository, times(1)).findByUserId(id);
+    }
+
+    @Test
+    @DisplayName("updateUserInfo updates only description if only description provided")
+    public void updateUserInfo_OnlyDescriptionProvided_UpdatesOnlyDescription() throws UserNotFoundException {
+        // given
+        var id = UUID.randomUUID();
+        UserUpdateDto onlyDesc = new UserUpdateDto(null, null, "desc");
+        given(userRepository.existsById(id)).willReturn(true);
+
+        // when
+        userService.updateUserInfo(id, onlyDesc);
+
+        // then
+        verify(userRepository, times(0)).updateDisplayedName(any(), any());
+        verify(userRepository, times(0)).updateAviUrl(any(), any());
+        verify(userRepository, times(1)).updateDescription(id, onlyDesc.getDescription());
+        verify(userRepository, times(1)).findByUserId(id);
     }
 }
