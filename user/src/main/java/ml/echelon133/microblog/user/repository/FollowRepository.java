5438a358d601a73f5b26db664018355eaecf4442
Mateusz <echelon133@gmail.com>
2023-03-15 12:42:50 +0100

    Implement custom queries which count following/followers of user

--- user/src/main/java/ml/echelon133/microblog/user/repository/FollowRepository.java
+++ user/src/main/java/ml/echelon133/microblog/user/repository/FollowRepository.java
@@ -3,5 +3,17 @@ package ml.echelon133.microblog.user.repository;
 import ml.echelon133.microblog.shared.user.Follow;
 import ml.echelon133.microblog.shared.user.FollowId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface FollowRepository extends JpaRepository<Follow, FollowId> {}
+import java.util.UUID;
+
+public interface FollowRepository extends JpaRepository<Follow, FollowId> {
+
+    @Query("SELECT count(*) FROM Follow f WHERE f.followId.followingUser = ?1 " +
+            "AND f.followId.followingUser <> f.followId.followedUser") // exclude mandatory self-follows from the results
+    long countUserFollowing(UUID userId);
+
+    @Query("SELECT count(*) FROM Follow f WHERE f.followId.followedUser = ?1 " +
+            "AND f.followId.followingUser <> f.followId.followedUser") // exclude mandatory self-follows from the results
+    long countUserFollowers(UUID userId);
+}
