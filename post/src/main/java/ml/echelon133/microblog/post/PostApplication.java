0888f4aeb6b277afabea14f2cde7d4d34bd70d7b
Mateusz <echelon133@gmail.com>
2023-03-17 18:02:53 +0100

    Add the main class of the user app

---
+++ post/src/main/java/ml/echelon133/microblog/post/PostApplication.java
@@ -0,0 +1,16 @@
+package ml.echelon133.microblog.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EntityScan({"ml.echelon133.microblog.shared"})
+@EnableDiscoveryClient
+public class PostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostApplication.class, args);
+    }
+}
