972da537bd47d9759030860dd90c892da8199b20
Mateusz <echelon133@gmail.com>
2023-03-17 16:14:12 +0100

    Add javadoc comments to FollowRepository methods

--- user/src/main/java/ml/echelon133/microblog/user/repository/FollowRepository.java
+++ user/src/main/java/ml/echelon133/microblog/user/repository/FollowRepository.java
@@ -12,24 +12,55 @@ import java.util.UUID;
 
 public interface FollowRepository extends JpaRepository<Follow, FollowId> {
 
+    /**
+     * Counts how many users are being followed by {@code userId}.
+     * @param userId id of the user who is on the 'following others' side of the relationship
+     * @return how many users are being followed
+     */
     @Query("SELECT count(*) FROM Follow f WHERE f.followId.followingUser = ?1 " +
             "AND f.followId.followingUser <> f.followId.followedUser") // exclude mandatory self-follows from the results
     long countUserFollowing(UUID userId);
 
+    /**
+     * Counts how many users are following {@code userId}.
+     * @param userId id of the user who is on the 'being followed' side of the relationship
+     * @return how many users are following
+     */
     @Query("SELECT count(*) FROM Follow f WHERE f.followId.followedUser = ?1 " +
             "AND f.followId.followingUser <> f.followId.followedUser") // exclude mandatory self-follows from the results
     long countUserFollowers(UUID userId);
 
+    /**
+     * Finds a {@link Page} of users who are being followed by {@code userId}.
+     * @param userId id of the user who is on the 'following others' side of the relationship
+     * @param pageable information about the page
+     * @return a page containing users
+     */
     @Query("SELECT NEW ml.echelon133.microblog.shared.user.UserDto(u.id, u.username, u.displayedName, u.aviURL, u.description) " +
             "FROM MBlog_User u JOIN Follow f ON u.id = f.followId.followedUser " +
             "WHERE f.followId.followingUser = ?1 AND f.followId.followingUser <> f.followId.followedUser")
     Page<UserDto> findAllUserFollowing(UUID userId, Pageable pageable);
 
+    /**
+     * Finds a {@link Page} of users who are following {@code userId}.
+     * @param userId id of the user who is on the 'being followed' side of the relationship
+     * @param pageable information about the page
+     * @return a page containing users
+     */
     @Query("SELECT NEW ml.echelon133.microblog.shared.user.UserDto(u.id, u.username, u.displayedName, u.aviURL, u.description) " +
             "FROM MBlog_User u JOIN Follow f ON u.id = f.followId.followingUser " +
             "WHERE f.followId.followedUser = ?1 AND f.followId.followingUser <> f.followId.followedUser")
     Page<UserDto> findAllUserFollowers(UUID userId, Pageable pageable);
 
+    /**
+     * Finds a {@link Page} of users who at the same time follow {@code targetUser} and are also being followed by
+     * {@code sourceUser}.
+     *
+     * @param sourceUser id of the user who wants to know their follows who also follow {@code targetUser}
+     * @param targetUser id of the user who is being followed by people known to {@code sourceUser}
+     * @param pageable information about the page
+     * @return a page containing users
+     */
     // finds intersection between those who follow 'targetUser' and those followed by 'sourceUser'
     // then returns all users from that intersection
     @Query("SELECT NEW ml.echelon133.microblog.shared.user.UserDto(u.id, u.username, u.displayedName, u.aviURL, u.description) " +
