52a68e6ea6305324594f48296c4e5af5952dd073
Mateusz <echelon133@gmail.com>
2023-03-18 22:37:00 +0100

    Implement service for publishing follows and unfollows

---
+++ user/src/main/java/ml/echelon133/microblog/user/queue/FollowPublisher.java
@@ -0,0 +1,26 @@
+package ml.echelon133.microblog.user.queue;
+
+import ml.echelon133.microblog.shared.queue.QueueTopic;
+import ml.echelon133.microblog.shared.user.follow.FollowInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FollowPublisher {
+
+    private final RedisTemplate<String, Object> followsTemplate;
+
+    @Autowired
+    public FollowPublisher(RedisTemplate<String, Object> followsTemplate) {
+        this.followsTemplate = followsTemplate;
+    }
+
+    public void publishCreateFollowEvent(FollowInfoDto dto) {
+        followsTemplate.convertAndSend(QueueTopic.CREATE_FOLLOW_TOPIC.getTopic(), dto);
+    }
+
+    public void publishRemoveFollowEvent(FollowInfoDto dto) {
+        followsTemplate.convertAndSend(QueueTopic.REMOVE_FOLLOW_TOPIC.getTopic(), dto);
+    }
+}
