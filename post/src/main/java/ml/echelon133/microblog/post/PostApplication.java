31390ade560c79c551e6dc25cbf51405d2db5a1c
Mateusz <echelon133@gmail.com>
2023-03-18 18:36:37 +0100

    Reduce entity scan range so that hibernate does not detect all entities, because it results in creation of tables which are not needed in post service's database

--- post/src/main/java/ml/echelon133/microblog/post/PostApplication.java
+++ post/src/main/java/ml/echelon133/microblog/post/PostApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EntityScan({"ml.echelon133.microblog.shared"})
+@EntityScan({"ml.echelon133.microblog.shared.post"})
 @EnableDiscoveryClient
 public class PostApplication {
 
