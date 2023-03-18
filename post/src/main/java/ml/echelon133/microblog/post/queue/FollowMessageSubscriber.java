021b4ddabdab81a426f633a3ff9b29b8189d9adb
Mateusz <echelon133@gmail.com>
2023-03-18 22:55:41 +0100

    Implement a MessageListener which listens for follows and mirrors them in the 'post' database

---
+++ post/src/main/java/ml/echelon133/microblog/post/queue/FollowMessageSubscriber.java
@@ -0,0 +1,56 @@
+package ml.echelon133.microblog.post.queue;
+
+import ml.echelon133.microblog.post.repository.FollowRepository;
+import ml.echelon133.microblog.shared.queue.QueueTopic;
+import ml.echelon133.microblog.shared.user.follow.Follow;
+import ml.echelon133.microblog.shared.user.follow.FollowId;
+import ml.echelon133.microblog.shared.user.follow.FollowInfoDto;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+
+@Service
+public class FollowMessageSubscriber implements MessageListener {
+
+    private static final Logger LOGGER = LogManager.getLogger(FollowMessageSubscriber.class);
+
+    private final FollowRepository followRepository;
+
+    @Autowired
+    public FollowMessageSubscriber(FollowRepository followRepository) {
+        this.followRepository = followRepository;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String topicName = new String(message.getChannel());
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(message.getBody()));
+            var follow = (FollowInfoDto)ois.readObject();
+            if (topicName.equals(QueueTopic.CREATE_FOLLOW_TOPIC.getTopic())) {
+                LOGGER.debug(
+                        String.format("Saving a follow of %s by user %s",
+                                follow.getFollowedUser(), follow.getFollowingUser()
+                        ));
+                followRepository.save(new Follow(follow.getFollowingUser(), follow.getFollowedUser()));
+            } else if (topicName.equals(QueueTopic.REMOVE_FOLLOW_TOPIC.getTopic())) {
+                LOGGER.debug(
+                        String.format("Removing a follow of %s by user %s",
+                                follow.getFollowedUser(), follow.getFollowingUser()
+                        ));
+                followRepository.deleteById(new FollowId(follow.getFollowingUser(), follow.getFollowedUser()));
+            } else {
+                LOGGER.warn("Received unexpected topic name: " + topicName);
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            LOGGER.error("Failed to deserialize a message from topic " + topicName);
+            e.printStackTrace();
+        }
+    }
+}
