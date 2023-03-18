a6b0dd5cbb0fbea48f96454d2b4d56e4dde3314b
Mateusz <echelon133@gmail.com>
2023-03-18 22:35:10 +0100

    Configure redis connection and RedisTemplate of 'user'

---
+++ user/src/main/java/ml/echelon133/microblog/user/queue/FollowQueueConfiguration.java
@@ -0,0 +1,33 @@
+package ml.echelon133.microblog.user.queue;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class FollowQueueConfiguration {
+
+    @Value("${spring.redis.host}")
+    String host;
+
+    @Value("${spring.redis.password}")
+    String password;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host);
+        config.setPassword(RedisPassword.of(password));
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
