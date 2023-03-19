83b03b5632a29c88b00806a0092a8642534a70a3
Mateusz <echelon133@gmail.com>
2023-03-19 16:53:13 +0100

    Implement TagNotFoundException

---
+++ post/src/main/java/ml/echelon133/microblog/post/exception/TagNotFoundException.java
@@ -0,0 +1,20 @@
+package ml.echelon133.microblog.post.exception;
+
+import ml.echelon133.microblog.shared.post.tag.Tag;
+
+import java.util.UUID;
+
+/**
+ * Exception thrown when {@link Tag} with specified {@link UUID} or name
+ * could not be found in the database.
+ */
+public class TagNotFoundException extends Exception {
+
+    public TagNotFoundException(String tagName) {
+        super(String.format("Tag '%s' could not be found", tagName));
+    }
+
+    public TagNotFoundException(UUID id) {
+        super(String.format("Tag with id %s could not be found", id.toString()));
+    }
+}
