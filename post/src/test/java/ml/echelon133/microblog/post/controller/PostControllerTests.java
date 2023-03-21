23166139b0f9d0ecb1a942114d599fcc8f5740b5
Mateusz <echelon133@gmail.com>
2023-03-21 13:45:36 +0100

    Implement tests of respondToPost controller method

--- post/src/test/java/ml/echelon133/microblog/post/controller/PostControllerTests.java
+++ post/src/test/java/ml/echelon133/microblog/post/controller/PostControllerTests.java
@@ -255,4 +255,112 @@ public class PostControllerTests {
                     .andExpect(jsonPath("$", hasEntry("uuid", randomPostUuid.toString())));
         }
     }
+
+    @Test
+    @DisplayName("respondToPost shows error when content's is empty")
+    public void respondToPost_ContentNotProvided_ReturnsExpectedError() throws Exception {
+        var parentPostId = UUID.randomUUID();
+        mvc.perform(
+                        post("/api/posts/" + parentPostId + "/responses")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(customBearerToken())
+                                .content("{}")
+                )
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.messages", hasSize(1)))
+                .andExpect(jsonPath("$.messages", hasItem("Post content not provided")));
+    }
+
+    @Test
+    @DisplayName("respondToPost shows error when content's length is invalid")
+    public void respondToPost_ContentLengthInvalid_ReturnsExpectedError() throws Exception {
+        var parentPostId = UUID.randomUUID();
+        var invalidContents = List.of(
+                // 301 characters (one too many to be valid)
+                "YjMN0eBEGvMi0OTUloGIktYL3KLkFRkxj0tiXdywScsti2eTKKzOot6deoC1zp99S6Qu5728HaQ1FneQ9gThdbk0AI26NRZwkK7BjUa5cqhv4OfSxqK5WApzM8oTSNnjRPymUAQCyY4R7ADG8DsG4Th2RlmsPvfDQMVHF2KduLhyKF3j4Xugg9FVpT06eRqFNh3KTR1XVin0TQvMbDSu22HKqrSwJ0HrkkoLFJCUaIwZLA7FBUXu4ySysrnOJeoadfi6EHUkTHFF88OIi2yIH3QqlHPL7emCRluJtHcCSEq1h",
+                // 400 characters (100 more than the upper limit)
+                "pUfmMvcnY2apDAN8GcE5sRCYMbyrJrS9pUgCXyHKNYPusWr4Dwlg2NSPa3yMJUj4atSuTIzqs4g7wujb4B1O3XkjJnoEgxw9xEhq04md9UfTZUSJfSTpIZNh3GB3Z7IPknxiBEfvq9Qr5FkjmEbVVF8yig0PXF0Jfqwr9nxoNSte6nqcnvkar64VImhv5bWEcivm1mXvS2OmpfbpNPSHVcJMJaZ7CJvVdnqQHLRWkXjjdH033xBvB2mBqva7gvT3MFwnEj0MJHMBGb1Rp8Exm0Xt4Fkqhxy95ZvW3OgsoLssUd0NZ59ZFnN5B8MnkCJg5O6OQUtReFKw617HmH78KX7cAwpgABdd6LrK76fqSTXwrucdw8kzawVEqVtqEP8mSekapg5cJRcbHblh"
+        );
+
+        for (var invalidContent : invalidContents) {
+            PostCreationDto dto = new PostCreationDto(invalidContent);
+            JsonContent<PostCreationDto> json = jsonPostCreationDto.write(dto);
+            mvc.perform(
+                            post("/api/posts/" + parentPostId + "/responses")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .with(customBearerToken())
+                                    .content(json.getJson())
+                    )
+                    .andExpect(status().isUnprocessableEntity())
+                    .andExpect(jsonPath("$.messages", hasSize(1)))
+                    .andExpect(jsonPath("$.messages", hasItem("Length of the post invalid")));
+        }
+    }
+
+    @Test
+    @DisplayName("respondToPost shows error when parent post does not exist")
+    public void respondToPost_ParentPostNotFound_ReturnsExpectedError() throws Exception {
+        var parentPostId = UUID.randomUUID();
+
+        var content = "test content";
+        PostCreationDto dto = new PostCreationDto(content);
+        JsonContent<PostCreationDto> json = jsonPostCreationDto.write(dto);
+
+        when(postService.createResponsePost(
+                eq(UUID.fromString(PRINCIPAL_ID)),
+                eq(parentPostId),
+                argThat(d -> d.getContent().equals(content))
+        )).thenThrow(new PostNotFoundException(parentPostId));
+
+        mvc.perform(
+                        post("/api/posts/" + parentPostId + "/responses")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(customBearerToken())
+                                .content(json.getJson())
+                )
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.messages", hasSize(1)))
+                .andExpect(jsonPath("$.messages", hasItem(
+                        String.format("Post with id %s could not be found", parentPostId))
+                ));
+    }
+
+    @Test
+    @DisplayName("respondToPost output ok when content's length is valid")
+    public void respondToPost_ContentLengthValid_ReturnsOk() throws Exception {
+        var parentPostId = UUID.randomUUID();
+        var contents = List.of(
+                // minimum length of a post
+                "a",
+                // 300 characters (maximum valid length)
+                "GwqYYnQfiZ3CZdlYxaNDkeAELhKuFZ3Zdv6jdwUTmy7WcDz5iY37wy41lj3WrsM64JZ17QGeGcVGC1b7BdW4gG98f4GN0HxeyWee9hyWNPT18UES2iO4xoF6OV4cMHD8eYiOfEv5cWXqmndzWqUvUNlKAdRthyn6uYgexeHmuKNgvYyEsYedLgZ8V8nSdKrUanOzzFrOE6gfqdKhMx76mChOsdRqa9TjGrlOq7QoS1cUydh76YDD0okyFEQkxxjQA8b3wm2lhQayLL7NCM4aJyALUVMJqiiisRzy3wUHUPZ5"
+        );
+
+        for (var content : contents) {
+            var randomPostUuid = UUID.randomUUID();
+            var post = new Post();
+            post.setId(randomPostUuid);
+
+            when(postService.createResponsePost(
+                    eq(UUID.fromString(PRINCIPAL_ID)),
+                    eq(parentPostId),
+                    argThat(dto -> dto.getContent().equals(content))
+            )).thenReturn(post);
+
+            PostCreationDto dto = new PostCreationDto(content);
+            JsonContent<PostCreationDto> json = jsonPostCreationDto.write(dto);
+            mvc.perform(
+                            post("/api/posts/" + parentPostId + "/responses")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .with(customBearerToken())
+                                    .content(json.getJson())
+                    )
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", hasEntry("uuid", randomPostUuid.toString())));
+        }
+    }
 }
