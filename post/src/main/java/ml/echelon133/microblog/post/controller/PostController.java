9edb140f85bf7464416cbbc311220fa8d6602bf9
Mateusz <echelon133@gmail.com>
2023-03-21 13:02:54 +0100

    Implement quotePost controller method

--- post/src/main/java/ml/echelon133/microblog/post/controller/PostController.java
+++ post/src/main/java/ml/echelon133/microblog/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package ml.echelon133.microblog.post.controller;
 
 import ml.echelon133.microblog.post.exception.InvalidPostContentException;
+import ml.echelon133.microblog.post.exception.PostNotFoundException;
 import ml.echelon133.microblog.post.service.PostService;
 import ml.echelon133.microblog.shared.post.PostCreationDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,25 @@ public class PostController {
         }
 
         return Map.of("uuid", postService.createPost(id, dto).getId());
+    }
+
+    @PostMapping("/{quotedPostId}/quotes")
+    public Map<String, UUID> quotePost(@Valid @RequestBody PostCreationDto dto, BindingResult result,
+                                       @PathVariable UUID quotedPostId,
+                                       @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal)
+            throws InvalidPostContentException, PostNotFoundException {
+
+        var id = UUID.fromString(Objects.requireNonNull(principal.getAttribute("token-owner-id")));
+
+        if (result.hasErrors()) {
+            List<String> errorMessages = result
+                    .getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            throw new InvalidPostContentException(errorMessages);
+        }
+
+        return Map.of("uuid", postService.createQuotePost(id, quotedPostId, dto).getId());
     }
 }
