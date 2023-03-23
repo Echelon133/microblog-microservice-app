93ae9c77120d0a1adef5265e9ae12500e94bddbc
Mateusz <echelon133@gmail.com>
2023-03-23 16:19:41 +0100

    Implement custom 'findMostRecentPostsOfUser' query

--- post/src/main/java/ml/echelon133/microblog/post/repository/PostRepository.java
+++ post/src/main/java/ml/echelon133/microblog/post/repository/PostRepository.java
@@ -2,6 +2,8 @@ package ml.echelon133.microblog.post.repository;
 
 import ml.echelon133.microblog.shared.post.Post;
 import ml.echelon133.microblog.shared.post.PostDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,4 +18,8 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     @Query("SELECT NEW ml.echelon133.microblog.shared.post.PostDto(p.id, p.dateCreated, p.content, p.authorId, p.quotedPost.id, p.parentPost.id) " +
             "FROM Post p WHERE p.id = ?1 AND p.deleted = false")
     Optional<PostDto> findByPostId(UUID id);
+
+    @Query("SELECT NEW ml.echelon133.microblog.shared.post.PostDto(p.id, p.dateCreated, p.content, p.authorId, p.quotedPost.id, p.parentPost.id) " +
+           "FROM Post p WHERE p.authorId = ?1 AND p.deleted = false ORDER BY p.dateCreated desc")
+    Page<PostDto> findMostRecentPostsOfUser(UUID userId, Pageable pageable);
 }
