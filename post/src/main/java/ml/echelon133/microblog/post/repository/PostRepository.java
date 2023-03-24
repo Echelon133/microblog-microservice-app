7dca56dfb7e7749576e781fa9a5168839111597e
Mateusz <echelon133@gmail.com>
2023-03-24 12:38:23 +0100

    Implement custom 'findMostRecentQuotesOfPost' and 'findMostRecentResponsesToPost' queries

--- post/src/main/java/ml/echelon133/microblog/post/repository/PostRepository.java
+++ post/src/main/java/ml/echelon133/microblog/post/repository/PostRepository.java
@@ -22,4 +22,12 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     @Query("SELECT NEW ml.echelon133.microblog.shared.post.PostDto(p.id, p.dateCreated, p.content, p.authorId, p.quotedPost.id, p.parentPost.id) " +
            "FROM Post p WHERE p.authorId = ?1 AND p.deleted = false ORDER BY p.dateCreated desc")
     Page<PostDto> findMostRecentPostsOfUser(UUID userId, Pageable pageable);
+
+    @Query("SELECT NEW ml.echelon133.microblog.shared.post.PostDto(p.id, p.dateCreated, p.content, p.authorId, p.quotedPost.id, p.parentPost.id) " +
+            "FROM Post p WHERE p.quotedPost.id = ?1 AND p.deleted = false ORDER BY p.dateCreated desc")
+    Page<PostDto> findMostRecentQuotesOfPost(UUID postId, Pageable pageable);
+
+    @Query("SELECT NEW ml.echelon133.microblog.shared.post.PostDto(p.id, p.dateCreated, p.content, p.authorId, p.quotedPost.id, p.parentPost.id) " +
+            "FROM Post p WHERE p.parentPost.id = ?1 AND p.deleted = false ORDER BY p.dateCreated desc")
+    Page<PostDto> findMostRecentResponsesToPost(UUID postId, Pageable pageable);
 }
