9c723799ce09a0b1f35db2e6194cd9d75e9d45aa
Mateusz <echelon133@gmail.com>
2023-03-27 13:04:31 +0200

    Define Clock bean to enable injecting fixed clocks into services during tests

--- post/src/main/java/ml/echelon133/microblog/post/PostApplication.java
+++ post/src/main/java/ml/echelon133/microblog/post/PostApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.Clock;
 
 @SpringBootApplication
 @EntityScan({"ml.echelon133.microblog.shared.post", "ml.echelon133.microblog.shared.user.follow"})
@@ -14,5 +17,10 @@ public class PostApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PostApplication.class, args);
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
     }
 }
