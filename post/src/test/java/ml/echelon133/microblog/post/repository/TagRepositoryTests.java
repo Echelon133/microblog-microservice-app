590e082d51247a29888d0b9ff484e0f128d180ea
Mateusz <echelon133@gmail.com>
2023-03-27 11:57:31 +0200

    Implement tests of custom 'findPopularTags' query method

---
+++ post/src/test/java/ml/echelon133/microblog/post/repository/TagRepositoryTests.java
@@ -0,0 +1,204 @@
+package ml.echelon133.microblog.post.repository;
+
+import ml.echelon133.microblog.shared.post.Post;
+import ml.echelon133.microblog.shared.post.tag.Tag;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    Disable kubernetes during tests to make local execution of tests possible.
+    If kubernetes is not disabled, tests won't execute at all because Spring will
+    fail to configure kubernetes when run outside it.
+ */
+@TestPropertySource(properties = "spring.cloud.kubernetes.enabled=false")
+@DataJpaTest
+@DisplayName("Tests of TagRepository")
+public class TagRepositoryTests {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    private void createTaggedPostOnDate(Set<String> tags, Date dateCreated) {
+        var foundTags = new HashSet<Tag>();
+        tags.forEach(tagName -> {
+            tagRepository.findByName(tagName).ifPresentOrElse(
+                    // tag present in the database
+                    foundTags::add,
+                    // tag not present in the database
+                    () -> {
+                        var tag = tagRepository.save(new Tag(tagName));
+                        foundTags.add(tag);
+                    }
+            );
+        });
+
+        // only post's date of creation and its tags matter for these tests
+        var post = new Post(UUID.randomUUID(), "some content", foundTags);
+
+        // setting 'dateCreated' before initial saving will result in that date being overwritten
+        // because of @DateCreated annotation that is modifying that field
+        var savedPost = postRepository.save(post);
+        // update the date after initial saving
+        savedPost.setDateCreated(dateCreated);
+        postRepository.save(savedPost);
+    }
+
+    @Test
+    @DisplayName("Custom findPopularTags returns an empty page when not a single post is tagged")
+    public void findPopularTags_NoTaggedPosts_ReturnsEmpty() {
+        // given
+        var oneMinuteAgo = Date.from(Instant.now().minus(1, ChronoUnit.MINUTES));
+        Set<String> noTags = Set.of();
+        createTaggedPostOnDate(noTags, oneMinuteAgo);
+        createTaggedPostOnDate(noTags, oneMinuteAgo);
+
+        // when
+        var pageable = Pageable.ofSize(5);
+        var sixHoursAgo = Date.from(Instant.now().minus(6, ChronoUnit.HOURS));
+        var now = new Date();
+        var result = tagRepository.findPopularTags(sixHoursAgo, now, pageable);
+
+        // then
+        assertEquals(0, result.getTotalElements());
+    }
+
+    @Test
+    @DisplayName("Custom findPopularTags returns page with tags in correct, descending popularity order")
+    public void findPopularTags_MultipleTaggedPosts_ReturnsElementsInCorrectOrder() {
+        // given
+        var oneMinuteAgo = Date.from(Instant.now().minus(1, ChronoUnit.MINUTES));
+        var tag1 = "test1";
+        var tag2 = "test2";
+        var tag3 = "test3";
+        var tag4 = "test4";
+        var tagGroup1 = Set.of(tag1);
+        var tagGroup2 = Set.of(tag1, tag2);
+        var tagGroup3 = Set.of(tag1, tag2, tag3);
+        var tagGroup4 = Set.of(tag1, tag2, tag3, tag4);
+
+        // create 10 posts for each tag group, so that:
+        // tag1 is used 40 times
+        // tag2 is used 30 times
+        // tag3 is used 20 times
+        // tag4 is used 10 times
+        for (int i = 0; i < 10; i++) {
+            createTaggedPostOnDate(tagGroup1, oneMinuteAgo);
+            createTaggedPostOnDate(tagGroup2, oneMinuteAgo);
+            createTaggedPostOnDate(tagGroup3, oneMinuteAgo);
+            createTaggedPostOnDate(tagGroup4, oneMinuteAgo);
+        }
+
+        // when
+        var pageable = Pageable.ofSize(5);
+        var sixHoursAgo = Date.from(Instant.now().minus(6, ChronoUnit.HOURS));
+        var now = new Date();
+        var result = tagRepository.findPopularTags(sixHoursAgo, now, pageable);
+
+        // then
+        assertEquals(4, result.getTotalElements());
+        var content = result.getContent();
+        assertEquals(tag1, content.get(0).getName());
+        assertEquals(tag2, content.get(1).getName());
+        assertEquals(tag3, content.get(2).getName());
+        assertEquals(tag4, content.get(3).getName());
+    }
+
+    @Test
+    @DisplayName("Custom findPopularTags returns page with only as many tags as specified in pageable")
+    public void findPopularTags_MultipleTaggedPosts_ReturnsNoMoreThanSpecified() {
+        // given
+        var oneMinuteAgo = Date.from(Instant.now().minus(1, ChronoUnit.MINUTES));
+        var tag1 = "test1";
+        var tag2 = "test2";
+        var tagGroup1 = Set.of(tag1);
+        var tagGroup2 = Set.of(tag1, tag2);
+
+        // create 10 posts for each tag group, so that:
+        // tag1 is used 20 times
+        // tag2 is used 10 times
+        for (int i = 0; i < 10; i++) {
+            createTaggedPostOnDate(tagGroup1, oneMinuteAgo);
+            createTaggedPostOnDate(tagGroup2, oneMinuteAgo);
+        }
+
+        // when
+        var pageable = Pageable.ofSize(1);
+        var sixHoursAgo = Date.from(Instant.now().minus(6, ChronoUnit.HOURS));
+        var now = new Date();
+        var result = tagRepository.findPopularTags(sixHoursAgo, now, pageable);
+
+        // then
+        assertEquals(1, result.getNumberOfElements());
+        var content = result.getContent();
+        assertEquals(tag1, content.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Custom findPopularTags calculates tag popularity only using posts created between specified dates")
+    public void findPopularTags_MultipleTaggedPosts_IgnoresPostsOutsideSpecifiedPeriod() {
+        // given
+        var tag1 = "test1";
+        var tag2 = "test2";
+        var tag3 = "test3";
+        var tag4 = "test4";
+        var tag5 = "test5";
+        var tag6 = "test6";
+        var tagGroup1 = Set.of(tag1);
+        var tagGroup2 = Set.of(tag5);
+        var tagGroup3 = Set.of(tag5, tag6);
+        var tagGroup4 = Set.of(tag1, tag2, tag3, tag4);
+
+        // create 5 posts for each tag group where all posts with:
+        // * tagGroup1 were made 6 hours and 1 minute ago
+        // * tagGroup2 were made 5 hours and 59 minutes ago
+        // * tagGroup3 were made 1 hour and 1 minute ago
+        // * tagGroup4 were made 59 minutes ago
+        //
+        // tagGroup1 | tagGroup2 | tagGroup3 | tagGroup4
+        // 6h01m ago | 5h59m ago | 1h01m ago | 59m ago
+        // XXXXXXXXXX|           |           |XXXXXXXXXX
+        //
+        // tested time period is between 6 hours ago and 1 hour ago, which should result in
+        // only posts tagged with tagGroup2 and tagGroup3 being taken into account while calculating
+        // tag popularity, so the expected result should be:
+        // * tag5 being used 10 times (5 times * two groups)
+        // * tag6 being used 5 times (5 times * one group)
+        // * other tags being completely ignored, because the posts which use them are not from the specified time period
+        //
+        var sixHoursAgo = Instant.now().minus(6, ChronoUnit.HOURS);
+        var oneHourAgo = Instant.now().minus(1, ChronoUnit.HOURS);
+        for (int i = 0; i < 5; i++) {
+            createTaggedPostOnDate(tagGroup1, Date.from(sixHoursAgo.minus(1, ChronoUnit.MINUTES)));
+            createTaggedPostOnDate(tagGroup2, Date.from(sixHoursAgo.plus(1, ChronoUnit.MINUTES)));
+            createTaggedPostOnDate(tagGroup3, Date.from(oneHourAgo.minus(1, ChronoUnit.MINUTES)));
+            createTaggedPostOnDate(tagGroup4, Date.from(oneHourAgo.plus(1, ChronoUnit.MINUTES)));
+        }
+
+        // when
+        var pageable = Pageable.ofSize(5);
+        var result = tagRepository.findPopularTags(Date.from(sixHoursAgo), Date.from(oneHourAgo), pageable);
+
+        // then
+        assertEquals(2, result.getNumberOfElements());
+        var content = result.getContent();
+        // only tag5 and tag6 had been posted in the time period which is given to the query
+        assertEquals(tag5, content.get(0).getName());
+        assertEquals(tag6, content.get(1).getName());
+    }
+}
