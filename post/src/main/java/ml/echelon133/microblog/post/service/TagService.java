f31106e32dfaeab8c7ab80b981d4628bd6904514
Mateusz <echelon133@gmail.com>
2023-03-27 13:17:41 +0200

    Implement 'findFiveMostPopularInLast' service method

--- post/src/main/java/ml/echelon133/microblog/post/service/TagService.java
+++ post/src/main/java/ml/echelon133/microblog/post/service/TagService.java
@@ -3,9 +3,16 @@ package ml.echelon133.microblog.post.service;
 import ml.echelon133.microblog.post.exception.TagNotFoundException;
 import ml.echelon133.microblog.post.repository.TagRepository;
 import ml.echelon133.microblog.shared.post.tag.Tag;
+import ml.echelon133.microblog.shared.post.tag.TagDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
 import java.util.regex.Pattern;
 
 @Service
@@ -14,10 +21,12 @@ public class TagService {
     public static Pattern HASHTAG_PATTERN = Pattern.compile("#([a-zA-Z0-9]{2,50})");
 
     private TagRepository tagRepository;
+    private Clock clock;
 
     @Autowired
-    public TagService(TagRepository tagRepository) {
+    public TagService(TagRepository tagRepository, Clock clock) {
         this.tagRepository = tagRepository;
+        this.clock = clock;
     }
 
     /**
@@ -28,5 +37,22 @@ public class TagService {
      */
     public Tag findByName(String name) throws TagNotFoundException {
         return tagRepository.findByName(name).orElseThrow(() -> new TagNotFoundException(name));
+    }
+
+    /**
+     * Finds at most five most popular tags in the (now minus {@code hours}) to (now) time period.
+     * @param hours how many hours back should the query go at most during calculation of the most popular tags
+     * @return a list of five tags which have been the most popular in the (now-{@code hours}) to (now) time period
+     * @throws IllegalArgumentException when {@code hours} is not in 1-24 range
+     */
+    public List<TagDto> findFiveMostPopularInLast(Integer hours) throws IllegalArgumentException {
+        if (hours > 24 || hours <= 0) {
+            throw new IllegalArgumentException("hours values not in 1-24 range are not valid");
+        }
+
+        return tagRepository.findPopularTags(
+                Date.from(Instant.now(clock).minus(hours, ChronoUnit.HOURS)),
+                Date.from(Instant.now(clock)),
+                Pageable.ofSize(5)).getContent();
     }
 }
