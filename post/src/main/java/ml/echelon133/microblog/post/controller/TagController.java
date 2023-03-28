5687603f76c73889dd605f346093e63e62e78e9c
Mateusz <echelon133@gmail.com>
2023-03-28 11:34:01 +0200

    Implement 'getMostRecentPostsInTag' controller method

--- post/src/main/java/ml/echelon133/microblog/post/controller/TagController.java
+++ post/src/main/java/ml/echelon133/microblog/post/controller/TagController.java
@@ -1,8 +1,12 @@
 package ml.echelon133.microblog.post.controller;
 
 import ml.echelon133.microblog.post.service.TagService;
+import ml.echelon133.microblog.shared.post.PostDto;
 import ml.echelon133.microblog.shared.post.tag.TagDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,5 +25,10 @@ public class TagController {
     @GetMapping("/popular")
     public List<TagDto> getPopularTags(@RequestParam(required = false, defaultValue = "1") Integer last) {
         return tagService.findFiveMostPopularInLast(last);
+    }
+
+    @GetMapping("/{name}/posts")
+    public Page<PostDto> getMostRecentPostsInTag(@PageableDefault(size = 20) Pageable pageable, @PathVariable String name) {
+        return tagService.findMostRecentPostsTagged(name, pageable);
     }
 }
