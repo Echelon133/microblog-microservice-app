7b3dad7a7bc0c1422ded73907688358b25ce966d
Mateusz <echelon133@gmail.com>
2023-03-28 09:06:35 +0200

    Implement 'getPopularTags' controller method

---
+++ post/src/main/java/ml/echelon133/microblog/post/controller/TagController.java
@@ -0,0 +1,25 @@
+package ml.echelon133.microblog.post.controller;
+
+import ml.echelon133.microblog.post.service.TagService;
+import ml.echelon133.microblog.shared.post.tag.TagDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tags")
+public class TagController {
+
+    private TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping("/popular")
+    public List<TagDto> getPopularTags(@RequestParam(required = false, defaultValue = "1") Integer last) {
+        return tagService.findFiveMostPopularInLast(last);
+    }
+}
