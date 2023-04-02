345d677f11a87383e35ea19c44d860f924e4a51f
Mateusz <echelon133@gmail.com>
2023-04-02 23:33:14 +0200

    Enable Jpa Auditing to make 'CreateDate' annotation work

--- notification/src/main/java/ml/echelon133/microblog/notification/NotificationApplication.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/NotificationApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan("ml.echelon133.microblog.shared.notification")
+@EnableJpaAuditing
 @EnableDiscoveryClient
 public class NotificationApplication {
 
