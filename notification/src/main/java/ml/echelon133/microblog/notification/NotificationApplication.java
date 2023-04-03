264b7976232bb063655828f2633ddd3f1a1168c7
Mateusz <echelon133@gmail.com>
2023-04-03 14:07:26 +0200

    Enable feign clients for NotificationApplication

--- notification/src/main/java/ml/echelon133/microblog/notification/NotificationApplication.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/NotificationApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan("ml.echelon133.microblog.shared.notification")
 @EnableJpaAuditing
 @EnableDiscoveryClient
+@EnableFeignClients
 public class NotificationApplication {
 
     public static void main(String[] args) {
