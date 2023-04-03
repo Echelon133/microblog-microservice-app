00e284d6c331f6cdeacff7a3bd699deba2f2cb3e
Mateusz <echelon133@gmail.com>
2023-04-03 14:08:31 +0200

    Define an ObjectMapper bean which contains modules required during deserialization of Page<> objects

--- notification/src/main/java/ml/echelon133/microblog/notification/NotificationApplication.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/NotificationApplication.java
@@ -1,10 +1,14 @@
 package ml.echelon133.microblog.notification;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.PageJacksonModule;
+import org.springframework.cloud.openfeign.support.SortJacksonModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -13,6 +17,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class NotificationApplication {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModules(
+                new PageJacksonModule(),
+                new SortJacksonModule()
+        );
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
