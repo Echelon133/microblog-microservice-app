705fd99436fdc66aaaf90c8a7db5386840553c63
Mateusz <echelon133@gmail.com>
2023-04-06 14:18:28 +0200

    Implement tests of 'findAllNotificationsOfUser' service method

--- notification/src/test/java/ml/echelon133/microblog/notification/service/NotificationServiceTests.java
+++ notification/src/test/java/ml/echelon133/microblog/notification/service/NotificationServiceTests.java
@@ -1,13 +1,19 @@
 package ml.echelon133.microblog.notification.service;
 
 import ml.echelon133.microblog.notification.repository.NotificationRepository;
+import ml.echelon133.microblog.shared.notification.Notification;
+import ml.echelon133.microblog.shared.notification.NotificationDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,5 +42,24 @@ public class NotificationServiceTests {
 
         // then
         assertEquals(100, count);
+    }
+
+    @Test
+    @DisplayName("findAllNotificationsOfUser correctly calls repository")
+    public void findAllNotificationsOfUser_ArgumentsProvided_CorrectlyCallsRepository() {
+        var userId = UUID.randomUUID();
+        var pageable = Pageable.ofSize(20);
+        var dto = new NotificationDto(UUID.randomUUID(), new Date(), UUID.randomUUID(), Notification.Type.MENTION, false);
+
+        // given
+        given(notificationRepository.findNotificationsOfUser(userId, pageable)).willReturn(
+                new PageImpl<>(List.of(dto), pageable, 1)
+        );
+
+        // when
+        var page = notificationService.findAllNotificationsOfUser(userId, pageable);
+
+        // then
+        assertEquals(dto, page.getContent().get(0));
     }
 }
