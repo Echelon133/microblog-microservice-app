d305e2d79c57189cc441ed3c769e4a3190cdae95
Mateusz <echelon133@gmail.com>
2023-04-06 16:00:36 +0200

    Implement queries 'readSingleNotification' and 'readAllNotificationsOfUser'

--- notification/src/main/java/ml/echelon133/microblog/notification/repository/NotificationRepository.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/repository/NotificationRepository.java
@@ -5,6 +5,7 @@ import ml.echelon133.microblog.shared.notification.NotificationDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +18,12 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     @Query("SELECT NEW ml.echelon133.microblog.shared.notification.NotificationDto(n.id, n.dateCreated, n.notificationSource, n.type, n.read) " +
             "FROM Notification n WHERE n.userToNotify = ?1 ORDER BY n.dateCreated DESC")
     Page<NotificationDto> findNotificationsOfUser(UUID userId, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Notification n SET n.read = true WHERE n.id = ?1 ")
+    int readSingleNotification(UUID notificationId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Notification n SET read = true WHERE n.userToNotify = ?1 ")
+    int readAllNotificationsOfUser(UUID userId);
 }
