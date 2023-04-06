0021d50626f245d9a8396a1e310e1188e7dda0e4
Mateusz <echelon133@gmail.com>
2023-04-06 14:21:12 +0200

    Implement 'findAllNotificationsOfUser' service method

--- notification/src/main/java/ml/echelon133/microblog/notification/service/NotificationService.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/service/NotificationService.java
@@ -1,7 +1,10 @@
 package ml.echelon133.microblog.notification.service;
 
 import ml.echelon133.microblog.notification.repository.NotificationRepository;
+import ml.echelon133.microblog.shared.notification.NotificationDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,6 +17,19 @@ public class NotificationService {
     @Autowired
     public NotificationService(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
+    }
+
+    /**
+     * Creates a {@link Page} containing projections of notifications of user {@code userId}.
+     * The most recent notifications appear first, and the state of a notification (i.e. whether it is marked as
+     * read or not) does not matter when it comes to ordering.
+     *
+     * @param userId id of the user whose notifications will be fetched
+     * @param pageable information about the wanted page
+     * @return a {@link Page} containing user's notifications
+     */
+    public Page<NotificationDto> findAllNotificationsOfUser(UUID userId, Pageable pageable) {
+        return notificationRepository.findNotificationsOfUser(userId, pageable);
     }
 
     /**
