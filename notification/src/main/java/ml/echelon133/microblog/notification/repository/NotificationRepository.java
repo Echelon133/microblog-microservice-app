6305a17e39bfa3010ad84868b4d964a8902ecaa7
Mateusz <echelon133@gmail.com>
2023-04-06 14:15:32 +0200

    Implement custom 'findNotificationsOfUser' query

--- notification/src/main/java/ml/echelon133/microblog/notification/repository/NotificationRepository.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/repository/NotificationRepository.java
@@ -1,7 +1,11 @@
 package ml.echelon133.microblog.notification.repository;
 
 import ml.echelon133.microblog.shared.notification.Notification;
+import ml.echelon133.microblog.shared.notification.NotificationDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,4 +13,8 @@ import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     int countByUserToNotifyAndReadFalse(UUID userId);
+
+    @Query("SELECT NEW ml.echelon133.microblog.shared.notification.NotificationDto(n.id, n.dateCreated, n.notificationSource, n.type, n.read) " +
+            "FROM Notification n WHERE n.userToNotify = ?1 ORDER BY n.dateCreated DESC")
+    Page<NotificationDto> findNotificationsOfUser(UUID userId, Pageable pageable);
 }
