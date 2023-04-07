df3bd283807a13cced48d94df4ef85dba978dc70
Mateusz <echelon133@gmail.com>
2023-04-07 22:04:38 +0200

    Write javadocs for NotificationRepository queries

--- notification/src/main/java/ml/echelon133/microblog/notification/repository/NotificationRepository.java
+++ notification/src/main/java/ml/echelon133/microblog/notification/repository/NotificationRepository.java
@@ -13,16 +13,41 @@ import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    /**
+     * Counts all unread notifications of a user with {@code userId}.
+     * @param userId id of the user whose unread notifications will be counted
+     * @return number of unread notifications
+     */
     int countByUserToNotifyAndReadFalse(UUID userId);
 
+    /**
+     * Finds a {@link Page} of notifications of a user.
+     *
+     * @param userId id of the user whose notifications will be fetched
+     * @param pageable all information about the wanted page
+     * @return a {@link Page} of notifications
+     */
     @Query("SELECT NEW ml.echelon133.microblog.shared.notification.NotificationDto(n.id, n.dateCreated, n.notificationSource, n.type, n.read) " +
             "FROM Notification n WHERE n.userToNotify = ?1 ORDER BY n.dateCreated DESC")
     Page<NotificationDto> findNotificationsOfUser(UUID userId, Pageable pageable);
 
+    /**
+     * Marks a single notification with {@code notificationId} as read.
+     *
+     * @param notificationId id of the notification to read
+     * @return how many notifications have been marked as read
+     */
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Notification n SET n.read = true WHERE n.id = ?1 ")
     int readSingleNotification(UUID notificationId);
 
+    /**
+     * Marks all notifications of a user with {@code userId} as read.
+     *
+     * @param userId id of the user whose all notification will be marked as read
+     * @return how many notifications have been marked as read
+     */
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Notification n SET read = true WHERE n.userToNotify = ?1 ")
     int readAllNotificationsOfUser(UUID userId);
