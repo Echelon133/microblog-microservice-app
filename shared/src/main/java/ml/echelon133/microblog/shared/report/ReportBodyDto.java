29e08098d37e8d5a76beb638a2b0736af1eb55f0
Mateusz <echelon133@gmail.com>
2023-04-10 16:24:20 +0200

    Implement DTO which is used for accepting and validating report information from users

---
+++ shared/src/main/java/ml/echelon133/microblog/shared/report/ReportBodyDto.java
@@ -0,0 +1,35 @@
+package ml.echelon133.microblog.shared.report;
+
+import ml.echelon133.microblog.shared.report.validator.ValidReason;
+import org.hibernate.validator.constraints.Length;
+
+public class ReportBodyDto {
+
+    @ValidReason
+    private String reason;
+
+    @Length(max = 300, message = "Length of the report's context invalid")
+    private String context;
+
+    public ReportBodyDto() {}
+    public ReportBodyDto(String reason, String context) {
+        this.reason = reason;
+        this.context = context;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
---
+++ shared/src/main/java/ml/echelon133/microblog/shared/report/validator/ValidReason.java
@@ -0,0 +1,19 @@
+package ml.echelon133.microblog.shared.report.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy= ReasonValidator.class)
+public @interface ValidReason {
+    String message() default "Report's 'reason' is not valid";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
