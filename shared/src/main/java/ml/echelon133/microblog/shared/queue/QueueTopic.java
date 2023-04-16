30f31eb3aa015c0ce194e55ba4f4b67c5b82ef57
Mateusz <echelon133@gmail.com>
2023-04-16 18:47:14 +0200

    Add javadoc to QueueTopic

--- shared/src/main/java/ml/echelon133/microblog/shared/queue/QueueTopic.java
+++ shared/src/main/java/ml/echelon133/microblog/shared/queue/QueueTopic.java
@@ -2,14 +2,34 @@ package ml.echelon133.microblog.shared.queue;
 
 import org.springframework.data.redis.listener.ChannelTopic;
 
+/**
+ * All {@link ChannelTopic}s used during communication between services.
+ */
 public class QueueTopic {
 
+    /**
+     * Topic containing information about users being followed.
+     */
     public final static ChannelTopic FOLLOW = new ChannelTopic("follow");
+
+    /**
+     * Topic containing information about users being unfollowed.
+     */
     public final static ChannelTopic UNFOLLOW = new ChannelTopic("unfollow");
 
+    /**
+     * Topic containing information about notifications.
+     */
     public final static ChannelTopic NOTIFICATION = new ChannelTopic("notification");
 
+    /**
+     * Topic containing information about posts being reported.
+     */
     public final static ChannelTopic REPORT = new ChannelTopic("report");
 
+    /**
+     * Topic containing information about what post needs to be deleted after a report
+     * has been accepted.
+     */
     public final static ChannelTopic REPORT_ACTION = new ChannelTopic("report-action");
 }
