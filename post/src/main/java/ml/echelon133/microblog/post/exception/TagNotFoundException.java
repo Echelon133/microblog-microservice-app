eab15a1345b48fc4ca2d7ed57c754bd672288cd6
Mateusz <echelon133@gmail.com>
2023-04-20 14:34:04 +0200

    Remove an unused constructor from TagNotFoundException

--- post/src/main/java/ml/echelon133/microblog/post/exception/TagNotFoundException.java
+++ post/src/main/java/ml/echelon133/microblog/post/exception/TagNotFoundException.java
@@ -5,16 +5,11 @@ import ml.echelon133.microblog.shared.post.tag.Tag;
 import java.util.UUID;
 
 /**
- * Exception thrown when a {@link Tag} with specified {@link UUID} or name
- * could not be found in the database.
+ * Exception thrown when a {@link Tag} with specified name could not be found in the database.
  */
 public class TagNotFoundException extends Exception {
 
     public TagNotFoundException(String tagName) {
         super(String.format("Tag '%s' could not be found", tagName));
-    }
-
-    public TagNotFoundException(UUID id) {
-        super(String.format("Tag with id %s could not be found", id.toString()));
     }
 }
