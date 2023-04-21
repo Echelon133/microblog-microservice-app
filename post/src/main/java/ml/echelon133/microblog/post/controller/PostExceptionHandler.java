56835d5b588470f9b63b1d0ee51d594db7d82c2a
Mateusz <echelon133@gmail.com>
2023-04-21 17:26:38 +0200

    Delete all exceptions superseded by ProvidedValuesInvalidException

--- post/src/main/java/ml/echelon133/microblog/post/controller/PostExceptionHandler.java
+++ post/src/main/java/ml/echelon133/microblog/post/controller/PostExceptionHandler.java
@@ -11,20 +11,6 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice(assignableTypes = {PostController.class, TagController.class, FeedController.class})
 public class PostExceptionHandler extends AbstractExceptionHandler {
 
-    @ExceptionHandler(value = InvalidPostContentException.class)
-    protected ResponseEntity<ErrorMessage> handleInvalidPostContentException(InvalidPostContentException ex,
-                                                                             WebRequest request) {
-        ErrorMessage error = new ErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, request, ex.getMessages());
-        return error.asResponseEntity();
-    }
-
-    @ExceptionHandler(value = InvalidReportContentException.class)
-    protected ResponseEntity<ErrorMessage> handleInvalidReportContentException(InvalidReportContentException ex,
-                                                                               WebRequest request) {
-        ErrorMessage error = new ErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, request, ex.getMessages());
-        return error.asResponseEntity();
-    }
-
     @ExceptionHandler(value = SelfReportException.class)
     protected ResponseEntity<ErrorMessage> handleSelfReportException(SelfReportException ex,
                                                                      WebRequest request) {
--- user/src/main/java/ml/echelon133/microblog/user/controller/UserExceptionHandler.java
+++ user/src/main/java/ml/echelon133/microblog/user/controller/UserExceptionHandler.java
@@ -1,7 +1,6 @@
 package ml.echelon133.microblog.user.controller;
 
 import ml.echelon133.microblog.shared.exception.AbstractExceptionHandler;
-import ml.echelon133.microblog.user.exception.UserDataInvalidException;
 import ml.echelon133.microblog.user.exception.UsernameTakenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +15,6 @@ public class UserExceptionHandler extends AbstractExceptionHandler {
     protected ResponseEntity<ErrorMessage> handleUsernameTakenException(UsernameTakenException ex,
                                                                         WebRequest request) {
         ErrorMessage error = new ErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, request, ex.getMessage());
-        return error.asResponseEntity();
-    }
-
-    @ExceptionHandler(value = UserDataInvalidException.class)
-    protected ResponseEntity<ErrorMessage> handleUserDataInvalidException(UserDataInvalidException ex,
-                                                                          WebRequest request) {
-        ErrorMessage error = new ErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, request, ex.getMessages());
         return error.asResponseEntity();
     }
 }
